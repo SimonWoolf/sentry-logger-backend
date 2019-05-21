d2340783779fc3c1cc747101a69423069004e46e
Simon Woolf <simon@ably.io>
2019-05-21 17:49:14 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SentryLoggerBackend.Mixfile do
   def project do
     [
       app: :sentry_logger_backend,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
